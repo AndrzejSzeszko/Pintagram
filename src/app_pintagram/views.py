bc0539ee20521baf83749e041b04b297329017a1
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-19 23:43:26 +0100

    add ListAllPhotosView

--- src/app_pintagram/views.py
+++ src/app_pintagram/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Photo
 
-# Create your views here.
+
+class ListAllPhotosView(generic.ListView):
+    model = Photo
+    template_name = 'app_pintagram/list_all_photos.html'
+    context_object_name = 'list_of_all_photos'
