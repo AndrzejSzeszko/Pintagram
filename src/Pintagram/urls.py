c3448eb9a099b4fe9f69fbbdbd62333cd2520047
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-19 23:41:39 +0100

    add path for ListAllPhotosView

--- src/Pintagram/urls.py
+++ src/Pintagram/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_pintagram import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.ListAllPhotosView.as_view(), name='list-of-all-photos'),
 ]
