6fdc0e62664a214209b59533c68fb5a9c0c3f36a
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-20 23:40:27 +0100

    add CommentForm

--- src/app_pintagram/forms.py
+++ src/app_pintagram/forms.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3.7
-
-
 from django import forms
-from .models import Photo
+from .models import (
+    Photo,
+    Comment
+)
 
 
 class PhotoForm(forms.ModelForm):
     model = Photo
     fields = '__all__'
+
+
+class CommentForm(forms.ModelForm):
+    model = Comment
+    fields = ['content']
