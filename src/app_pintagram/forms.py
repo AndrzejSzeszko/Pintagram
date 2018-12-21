fc497a021195b43653901955fa3ab08eda757d66
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-21 03:42:21 +0100

    add SignInForm and classes 'Meta' to all models

--- src/app_pintagram/forms.py
+++ src/app_pintagram/forms.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3.7
 from django import forms
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from .models import (
     Photo,
     Comment
 )
 
 
+class SignInForm(UserCreationForm):
+    class Meta:
+        model  = get_user_model()
+        fields = ['username', 'email', 'password1', 'password2']
+
+
 class PhotoForm(forms.ModelForm):
-    model = Photo
-    fields = '__all__'
+    class Meta:
+        model  = Photo
+        fields = '__all__'
 
 
 class CommentForm(forms.ModelForm):
-    model = Comment
-    fields = ['content']
+    class Meta:
+        model  = Comment
+        fields = ['content']
