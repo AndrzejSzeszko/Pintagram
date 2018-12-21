e386c7a975f656501084e13a4429a9e5130a48cf
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-21 23:27:12 +0100

    some quick'n'dirty registering of custom user model

--- src/app_pintagram/admin.py
+++ src/app_pintagram/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .models import (
-    User,
     Photo,
     Comment
 )
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    class Meta:
+        model = get_user_model()
+
+
+admin.site.register(get_user_model(), CustomUserAdmin)
 admin.site.register(Photo)
 admin.site.register(Comment)
