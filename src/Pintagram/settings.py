5cfab65063def09c14b873dbccc10e7614eed32e
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-21 03:40:49 +0100

    add LOGIN_URL and LOGIN_REDIRECT_URL

--- src/Pintagram/settings.py
+++ src/Pintagram/settings.py
@@ -136,3 +136,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# logging in stuff
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'list-of-all-photos'
