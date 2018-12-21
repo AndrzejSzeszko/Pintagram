ff46499e8ec150017d3bbf9d301d25849b4e7305
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-21 03:45:27 +0100

    minor refactorization

--- src/app_pintagram/models.py
+++ src/app_pintagram/models.py
@@ -1,13 +1,16 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import (
+    models as auth_models,
+    get_user_model
+)
 from django.db import models
 from django.conf import settings
 
 
 def get_sentinel_user():
-    return settings.AUTH_USER_MODEL.objects.get_or_create(username='Deleted user')[0]
+    return get_user_model().objects.get_or_create(username='Deleted_user')[0]
 
 
-class User(AbstractUser):
+class User(auth_models.AbstractUser):
     profile_photo = models.ImageField(upload_to='profile_pics', default='default_user.jpg')
 
 
@@ -21,8 +24,8 @@ class Photo(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(get_sentinel_user))
-    photo = models.ForeignKey('Photo', on_delete=models.CASCADE)
-    content = models.TextField(max_length=256)
+    author            = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(get_sentinel_user))
+    photo             = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    content           = models.TextField(max_length=256)
     creation_datetime = models.DateTimeField(auto_now_add=True)
 
