360cae86bb53b2c09a3c31ae82cfebf2dc042b6f
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-21 03:43:24 +0100

    register 'User', 'Photo', 'Comment' models

--- src/app_pintagram/admin.py
+++ src/app_pintagram/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import (
+    User,
+    Photo,
+    Comment
+)
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Photo)
+admin.site.register(Comment)
