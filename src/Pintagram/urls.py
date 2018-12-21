18483c43b27be938ab9e061741ab193480a25ef0
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-21 03:40:05 +0100

    add 'sign_in', 'login', 'logout' paths

--- src/Pintagram/urls.py
+++ src/Pintagram/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from app_pintagram import views
 from django.conf import settings
@@ -22,5 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.ListAllPhotosView.as_view(), name='list-of-all-photos'),
-
+    path('sign_in/', views.SignInView.as_view(), name='sign-in'),
+    path('login/', auth_views.LoginView.as_view(template_name='app_pintagram/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='app_pintagram/logout.html'), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
