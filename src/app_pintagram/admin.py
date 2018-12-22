c0a833496f826cea6105ac9d298779043ba44bd2
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-23 00:07:34 +0100

    make adding photos in CustomUserAdmin working

--- src/app_pintagram/admin.py
+++ src/app_pintagram/admin.py
@@ -2,16 +2,20 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .models import (
-    Photo,
+    Post,
     Comment
+)
+from .forms import (
+    UpdateCustomUserForm,
+    SignInForm
 )
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    class Meta:
-        model = get_user_model()
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+    form = UpdateCustomUserForm
 
 
 admin.site.register(get_user_model(), CustomUserAdmin)
-admin.site.register(Photo)
+admin.site.register(Post)
 admin.site.register(Comment)
