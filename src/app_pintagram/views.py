412351e0f09516c3ecf961a1efbf4e83b2fc5c0b
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-23 00:03:23 +0100

    add DeleteCustomUser, CreatePostView, PostDetailsView

--- src/app_pintagram/views.py
+++ src/app_pintagram/views.py
@@ -4,19 +4,20 @@ from django.shortcuts import (
 )
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 from django.views import generic
 from django.conf import settings
-from .models import Photo
+from .models import Post
 from .forms import (
-    PhotoForm,
+    PostForm,
     CommentForm,
     SignInForm,
     UpdateCustomUserForm
 )
 
 
-class ListAllPhotosView(generic.ListView):
-    model               = Photo
+class ListAllPostsView(generic.ListView):
+    model               = Post
     template_name       = 'app_pintagram/list_all_photos.html'
     context_object_name = 'list_of_all_photos'
 
@@ -37,7 +38,35 @@ class UpdateCustomUserView(generic.UpdateView):
         return reverse_lazy('user-details', kwargs={'pk': self.get_object().pk})
 
 
-class UserDetailsView(generic.DetailView):
+class CustomUserDetailView(generic.DetailView):
     model               = get_user_model()
     template_name       = 'app_pintagram/user_details.html'
     context_object_name = 'user_details'
+
+
+class DeleteCustomUser(generic.DeleteView):
+    model         = get_user_model()
+    template_name = 'app_pintagram/user_delete.html'
+    success_url   = reverse_lazy('login')
+
+
+class CreatePostView(generic.CreateView):
+    model         = Post
+    template_name = 'app_pintagram/post_create.html'
+    form_class    = PostForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        rsp = super().form_valid(form)
+        messages.success(self.request, 'Post successfully created!')
+        return rsp
+
+    def form_invalid(self, form):
+        rsp = super().form_invalid(form)
+        messages.error(self.request, 'Form wasn\'t filled correctly.')
+        return rsp
+
+
+class PostDetailsView(generic.DetailView):
+    model         = Post
+    template_name = 'app_pintagram/post_details.html'
